ca395c656ed4c58b8650c880016473fc7960034a
mpuellesc <mpuellesc@unitru.edu.pe>
2021-12-17 13:56:55 -0500

    Modificacion prueba

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -3,7 +3,7 @@
   Launch demo modal
 </button>
 
-<!-- Modal -->
+<!-- Modal xdddd ffffffffffffffffffffffffffffffffffffff-->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
